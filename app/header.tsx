8660a0b60b9002f37428919ebcd8b91f9f1f85f1
nethaka-sades <sades.dev01@gmail.com>
2025-07-30 22:54:29 +0530

    small change

--- app/header.tsx
+++ app/header.tsx
@@ -1,3 +1,29 @@
+/*  
+ *  
+ *   Copyright 2025 Nethaka De Saram
+ *  
+ *  Licensed under the Apache License, Version 2.0 (the 'License');
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *  
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an 'AS IS' BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *  
+ *  
+ *  
+ *  
+ *  Created on Mon Jul 28 2025
+ *  
+ *  This file is directly inspired by Nim Portfolio Template by Julien Thibeaut
+ *  https://github.com/ibelick/nim 
+ * 
+ */
+
 'use client'
 import { TextEffect } from '@/components/ui/text-effect'
 import Link from 'next/link'
@@ -16,7 +42,7 @@ export function Header() {
           className="text-zinc-600 dark:text-zinc-500"
           delay={0.5}
         >
-          I tweet on X.
+          Sometimes they even works.
         </TextEffect>
       </div>
     </header>
